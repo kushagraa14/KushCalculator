16146cd826495c1ed33c3a9849352dea59e35f3f
kushagraa14 <kushagra@clevertap.com>
2022-06-12 21:02:10 +0530

    Initial project setup

---
+++ KushCalculator/KushCalculator.h
@@ -0,0 +1,18 @@
+//
+//  KushCalculator.h
+//  KushCalculator
+//
+//  Created by Kushagra Mishra on 12/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KushCalculator.
+FOUNDATION_EXPORT double KushCalculatorVersionNumber;
+
+//! Project version string for KushCalculator.
+FOUNDATION_EXPORT const unsigned char KushCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KushCalculator/PublicHeader.h>
+
+
---
+++ KushCalculatorExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KushCalculatorExamples
+//
+//  Created by Kushagra Mishra on 12/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
