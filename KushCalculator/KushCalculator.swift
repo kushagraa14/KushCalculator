ec2f039f6908e9d8086e19c7cd1ae6b5fa93f525
kushagraa14 <kushagra@clevertap.com>
2022-06-12 22:38:52 +0530

    Added test cases

---
+++ KushCalculator/KushCalculator.swift
@@ -0,0 +1,11 @@
+public final class KushCalculator {
+    let name = "KushCalculator"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- KushCalculatorTests/KushCalculatorTests.swift
+++ KushCalculatorTests/KushCalculatorTests.swift
@@ -10,24 +10,17 @@ import XCTest
 
 class KushCalculatorTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var kushCalculator: KushCalculator!
+    
+    override func setUp() {
+        kushCalculator = KushCalculator()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(kushCalculator.add(a:1, b:1), 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSub() {
+        XCTAssertEqual(kushCalculator.sub(a:1, b:1), 0)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
