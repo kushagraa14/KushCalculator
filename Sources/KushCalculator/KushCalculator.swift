32ee27ed8e7aacbd09e1a9ae42f9e28b654005d1
kushagraa14 <kushagra@clevertap.com>
2022-06-13 02:02:38 +0530

    Added SPM dependency

---
+++ Sources/KushCalculator/KushCalculator.swift
@@ -0,0 +1,6 @@
+public struct KushCalculator {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/KushCalculatorTests/KushCalculatorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import KushCalculator
+
+final class KushCalculatorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(KushCalculator().text, "Hello, World!")
+    }
+}
