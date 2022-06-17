58272c8d1eeb456ed44122b55157d88239138c97
Kushagra Mishra <96417921+kushagraa14@users.noreply.github.com>
2022-06-17 10:57:38 +0530

    Updated carthage.yml

--- scripts/carthage.sh
+++ scripts/carthage.sh
@@ -1,17 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/sh -e
+echo "Carthage wrapper"
+echo "Applying Xcode 12 workaround..."
+xcconfig="/tmp/xc12-carthage.xcconfig"
 
-# carthage.sh
-# Usage example: ./carthage.sh build --platform iOS
+# Xcode 12.x
+echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x86_64__XCODE_1200 = arm64 arm64e armv7 armv7s armv6 armv8' > $xcconfig
 
-set -euo pipefail
-
-xcconfig=$(mktemp /tmp/static.xcconfig.XXXXXX)
-trap 'rm -f "$xcconfig"' INT TERM HUP EXIT
-
-# For Xcode 12 make sure EXCLUDED_ARCHS is set to arm architectures otherwise
-# the build will fail on lipo due to duplicate architectures.
-echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x86_64__XCODE_1200 = arm64 arm64e armv7 armv7s armv6 armv8' >> $xcconfig
+# General stuff
 echo 'EXCLUDED_ARCHS = $(inherited) $(EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_$(EFFECTIVE_PLATFORM_SUFFIX)__NATIVE_ARCH_64_BIT_$(NATIVE_ARCH_64_BIT)__XCODE_$(XCODE_VERSION_MAJOR))' >> $xcconfig
-
+echo 'ONLY_ACTIVE_ARCH=NO' >> $xcconfig
+echo 'VALID_ARCHS = $(inherited) x86_64' >> $xcconfig
 export XCODE_XCCONFIG_FILE="$xcconfig"
-carthage "$@"
+echo "Workaround applied. xcconfig here: $XCODE_XCCONFIG_FILE"
+
+carthage $@
